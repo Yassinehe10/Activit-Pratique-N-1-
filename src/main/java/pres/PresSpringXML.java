fbc4b6abb06cb4ae6470a4594cbfb4fbeb57219b
unknown <you@example.com>
2024-03-30 21:36:30 +0000

    First Commit

---
+++ src/main/java/pres/PresSpringXML.java
@@ -0,0 +1,12 @@
+package pres;
+
+import metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PresSpringXML {
+    public static void main(String[] args){
+        ApplicationContext context= new ClassPathXmlApplicationContext("applicationcontext.xml");
+        IMetier metier=(IMetier) context.getBean("metier");
+    }
+}
